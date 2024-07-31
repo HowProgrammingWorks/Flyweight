0723664bd1f16efe0b9fe62e18ca6b38b8b9f535
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-07-31 22:13:53 +0300

    Add JavaScript implementation

---
+++ JavaScript/3-js-way.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const timers = new Map();
+
+const free = ({ listeners, instance }) => (callback) => {
+  listeners.delete(callback);
+  if (listeners.length === 0) {
+    clearInterval(instance);
+    timers.delete(this);
+  }
+};
+
+const interval = (msec, callback) => {
+  let timer = timers.get(msec);
+  if (timer) return timer;
+  const listeners = new Set();
+  const instance = setInterval(() => {
+    timer.listeners.forEach((callback) => callback());
+  }, msec);
+  timer = { listeners, instance };
+  timers.set(msec, timer);
+  listeners.add(callback);
+  return Object.assign(timer, { remove: free(timer) });
+};
+
+module.exports = interval;
