6869ef3e8431aca48355ada28a02884b9eace3be
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-08-01 18:44:21 +0300

    Add prototype-programming example

---
+++ JavaScript/3-prototype.js
@@ -0,0 +1,40 @@
+'use strict';
+
+class Interval {
+  static timers = new Map();
+
+  constructor(msec, callback) {
+    this.callback = callback;
+    const timer = Interval.timers.get(msec);
+    if (timer) {
+      Object.setPrototypeOf(this, timer);
+    } else {
+      this.listeners = new Set();
+      this.instance = setInterval(() => {
+        for (const callback of this.listeners.values()) {
+          callback();
+        }
+      }, msec);
+      Interval.timers.set(msec, this);
+    }
+    this.listeners.add(this.callback);
+  }
+
+  remove() {
+    this.listeners.delete(this.callback);
+    if (this.listeners.length === 0) {
+      clearInterval(this.instance);
+      Interval.timers.delete(this);
+    }
+  }
+}
+
+// Usage
+
+const COUNT = 1000000;
+for (let i = 0; i < COUNT; i++) {
+  new Interval(1000, () => {});
+  new Interval(2000, () => {});
+}
+const memory = process.memoryUsage();
+console.log({ memory });
